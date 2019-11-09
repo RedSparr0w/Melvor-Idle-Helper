4d8ec930389cf2002b508c6a2f12d8b57de05fe7
RedSparr0w <admin@redsparr0w.com>
2019-11-09 23:44:26 +1300

    commented code, maybe useful for later

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -6,7 +6,7 @@ document.body.appendChild(script);
  * DISABLE THE CLOUD SAVE STUFF FOR NOW
  * needs a lot more work to correctly detect when to overwrite your data
  *
- 
+
 // Load latest save
 chrome.storage.sync.get(null, function(all){
 	Object.keys(all).forEach(key=>{
@@ -21,4 +21,25 @@ setInterval(()=>{
 		console.debug('[Melvor Idle Helper] Saved Game');
 	});
 }, 5e3);
-*/
+//*/
+
+
+
+/*
+ * Get current cloud save from melvor cloud
+ *
+
+// NOT WORKING CURRENTLY AS SESSION IS NOT PERSISTANT
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.responseURL.includes('actions.php') && this.readyState == 4 && this.status == 200) {
+	    console.log(this);
+    }
+};
+xhttp.open("POST", "cloud/actions.php", true);
+xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+exportSave(false);
+mySaveData = document.getElementById("exportSaveField").value;
+xhttp.send("cloud-btn-view=");
+//*/
